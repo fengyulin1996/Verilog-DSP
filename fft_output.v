609554709e9bc168df0b523ef72bd18f9bf385da
fengyulin1996 <33176939+fengyulin1996@users.noreply.github.com>
2017-12-03 19:38:29 +0800

    Update fft_output.v

--- fft_output.v
+++ fft_output.v
@@ -4,21 +4,21 @@ function : control output(when to take input
 与串行输出设备的简单接口，由接收设备决定主机是否向外输出
 */
 module fft_output(
-    input wire rst,
+    input wire rstn,
     input wire req_i,
     input wire ans_i,
     input wire en,
     input wire [15:0] data_iR,
     input wire [15:0] data_iJ,
-    output wire req_o,
-    output wire ans_o,
+    output reg req_o,
+    output reg ans_o,
     output reg [15:0] data_oR,
     output reg [15:0] data_oJ
     );
-    assign req_o = req_i;
-    assign ans_o = ans_i;
     always @(*) begin
-        if(rst == 1'b0) begin
+        req_o = req_i;
+        ans_o = ans_i;
+        if(rstn == 1'b0) begin
             data_oR <= 16'h0000;
             data_oJ <= 16'h0000;
         end
