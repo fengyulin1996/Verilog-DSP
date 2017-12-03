7b501f3cb0fba18acf0597cb340abf666641370f
fengyulin1996 <33176939+fengyulin1996@users.noreply.github.com>
2017-12-03 19:37:55 +0800

    Update fft_input.v

--- fft_input.v
+++ fft_input.v
@@ -2,9 +2,10 @@
 /*
 function : control input(when to take input
 与串行输入设备的简单接口，起到根据主机的状态决定是否传递数据进入的作用，en=1允许输入
+端口连接时输入可以用reg，输出必须用wire
 */
 module fft_input(
-    input wire rst,
+    input wire rstn,
     input wire req_i,
     input wire ans_i,
     input wire en,
@@ -17,7 +18,7 @@ module fft_input(
     always @(*) begin
         req_o = req_i;
         ans_o = ans_i;
-        if(rst == 1'b0) begin
+        if(rstn == 1'b0) begin
             data_o <= 16'h0000;
         end
         else if(en == 1'b1) begin
