0451cdc44fde84f11269331241915a559bef065b
fengyulin1996 <33176939+fengyulin1996@users.noreply.github.com>
2017-12-03 19:39:49 +0800

    Update tb_input_top.v

--- tb_input_top.v
+++ tb_input_top.v
@@ -1,16 +1,17 @@
 `timescale 1ns / 1ps
-module tb_input_top6();
-reg clk,rst;
+module tb_input_top11();
+reg clk,rstn;
 reg req;
 reg [15:0] data;    //input
 
 initial begin
     clk = 1'b0;
-    rst = 1'b0;    //复位
+    rstn = 1'b0;    //复位
     req = 1'b1;    //打开输入请求
     data = 16'h7fff;    //输入1
-    #40 rst = 1'b1;    // 解除复位
-    #40 data = 16'h0000;    //输入0
+    #80 rstn = 1'b1;    // 解除复位
+    data = 16'h7fff;
+    #400 data = 16'h0000;    //输入0
 end
 
 always begin
@@ -18,7 +19,7 @@ always begin
 end
 fft_top fft_top
     (.clk(clk),
-     .rst(rst),
+     .rstn(rstn),
      .req_i(req),
      .data_i(data)
      );
